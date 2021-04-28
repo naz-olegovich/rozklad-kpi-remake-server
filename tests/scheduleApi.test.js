0710dc01c1149e5a60d01d00e83a15ecce4f9e3b
Nazar Chopyk <61637619+naz-olegovich@users.noreply.github.com>
2021-04-28 10:26:24 +0300

    Add tests for teachers api

--- tests/scheduleApi.test.js
+++ tests/scheduleApi.test.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 const { dbUrl } = require('../config');
 const Groups = require('../src/models/Group');
-const Teacher = require('../src/models/Teacher');
+const Teachers = require('../src/models/Teacher');
 const jsonProcessing = require('../src/jsonProcessing');
 
 beforeAll(async () => {
@@ -36,8 +36,8 @@ describe('testing groups api" ', () => {
         expect(res.body).toEqual(JSON.parse(JSON.stringify(expectedFromDb)));
     });
 
-    it('GET /groups/:id/lessons(by id) - success', async () => {
 
+    it('GET /groups/:id/lessons(by id) - success', async () => {
         const res = await request(app).get('/api/groups/537/lessons'); //uses the request function that calls on express app instance
         const query = { $or: [{ id: 537 }] };
         const projection = { _id: 0, __v: 0 };
@@ -59,7 +59,7 @@ describe('testing groups api" ', () => {
         });
     });
 
-    it('GET /groups/:id/lessons (by name) - success', async () => {
+    it('GET /groups/:name/lessons (by name) - success', async () => {
         const res = await request(app).get(`/api/groups/${encodeURI('ів-91')}/lessons`); //uses the request function that calls on express app instance
         const query = { $or: [{ name: 'ів-91' }] };
         const projection = { _id: 0, __v: 0 };
@@ -70,7 +70,7 @@ describe('testing groups api" ', () => {
         expect(res.body).toEqual(JSON.parse(JSON.stringify(expectedFromDb)));
     });
 
-    it('GET /groups/:id/lessons (by name) - error', async () => {
+    it('GET /groups/:name/lessons (by name) - error', async () => {
         const res = await request(app).get('/api/groups/some-unexpected-name/lessons');
         expect(res.statusCode).toEqual(404);
         expect(res.body).toEqual({
@@ -79,8 +79,9 @@ describe('testing groups api" ', () => {
         });
     });
 
-    it('GET /groups/:id/timetable (by name) - success', async () => {
-        const res = await request(app).get(`/api/groups/${encodeURI('ів-91')}/timetable`); //uses the request function that calls on express app instance
+
+    it('GET /groups/:id/timetable (by id) - success', async () => {
+        const res = await request(app).get('/api/groups/537/timetable'); //uses the request function that calls on express app instance
         const query = { $or: [{ id: 537 }] };
         const projection = { _id: 0, __v: 0 };
         const { id, name, prefix, okr, type, lessons } = await Groups.findOne(query, projection);
@@ -91,4 +92,140 @@ describe('testing groups api" ', () => {
         expect(res.body).toEqual({ id, name, prefix, okr, type, weeks });
     });
 
+    it('GET /groups/:id/timetable (by id) - error', async () => {
+        const res = await request(app).get('/api/groups/-999999/timetable');
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual({
+            'message': 'Group not found',
+            'statusCode': 404
+        });
+    });
+
+    it('GET /groups/:name/timetable (by name) - success', async () => {
+        const res = await request(app).get(`/api/groups/${encodeURI('ів-91')}/timetable`); //uses the request function that calls on express app instance
+        const query = { $or: [{ name: 'ів-91' }] };
+        const projection = { _id: 0, __v: 0 };
+        const { id, name, prefix, okr, type, lessons } = await Groups.findOne(query, projection);
+        const { weeks } = jsonProcessing.createTimetable(lessons);
+
+        expect(mongoose.connection.readyState).toEqual(1); //1: connected
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({ id, name, prefix, okr, type, weeks });
+    });
+
+    it('GET /groups/:name/timetable (by name) - error', async () => {
+        const res = await request(app).get('/api/groups/some-unexpected-name/timetable');
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual({
+            'message': 'Group not found',
+            'statusCode': 404
+        });
+    });
+});
+
+
+describe('testing teachers api" ', () => {
+    it('GET /api/teachers (with limit)', async () => {
+        const res = await request(app).get('/api/teachers?limit=500'); //uses the request function that calls on express app instance
+        const expectedFromDb = await Teachers.find({}, { _id: 0, __v: 0, lessons: 0 }).limit(500);
+
+        expect(mongoose.connection.readyState).toEqual(1); //1: connected
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual(JSON.parse(JSON.stringify(expectedFromDb)));
+    });
+
+    it('GET /api/teachers (with offset)', async () => {
+        const res = await request(app).get('/api/teachers?offset=500'); //uses the request function that calls on express app instance
+        const expectedFromDb = await Teachers.find({}, { _id: 0, __v: 0, lessons: 0 }).limit(500).skip(500);
+
+        expect(mongoose.connection.readyState).toEqual(1); //1: connected
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual(JSON.parse(JSON.stringify(expectedFromDb)));
+    });
+
+
+    it('GET /teachers/:id/lessons(by id) - success', async () => {
+        const res = await request(app).get('/api/teachers/294/lessons'); //uses the request function that calls on express app instance
+        const query = { $or: [{ id: 294 }] };
+        const projection = { _id: 0, __v: 0 };
+        const expectedFromDb = await Teachers.findOne(query, projection);
+
+        expect(mongoose.connection.readyState).toEqual(1); //1: connected
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual(JSON.parse(JSON.stringify(expectedFromDb)));
+    });
+
+    it('GET /teachers/:id/lessons (by id) - error', async () => {
+
+        const res = await request(app).get('/api/teachers/-1/lessons');
+
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual({
+            'message': 'Teacher not found',
+            'statusCode': 404
+        });
+    });
+
+    it('GET /teachers/:name/lessons (by name) - success', async () => {
+        const res = await request(app).get(`/api/teachers/${encodeURI('Болдак Андрій Олександрович')}/lessons`); //uses the request function that calls on express app instance
+        const query = { $or: [{ name: 'Болдак Андрій Олександрович' }] };
+        const projection = { _id: 0, __v: 0 };
+        const expectedFromDb = await Teachers.findOne(query, projection);
+
+        expect(mongoose.connection.readyState).toEqual(1); //1: connected
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual(JSON.parse(JSON.stringify(expectedFromDb)));
+    });
+
+    it('GET /teachers/:name/lessons (by name) - error', async () => {
+        const res = await request(app).get('/api/teachers/some-unexpected-name/lessons');
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual({
+            'message': 'Teacher not found',
+            'statusCode': 404
+        });
+    });
+
+
+    it('GET /teachers/:id/timetable (by id) - success', async () => {
+        const res = await request(app).get('/api/teachers/294/timetable'); //uses the request function that calls on express app instance
+        const query = { $or: [{ id: 294 }] };
+        const projection = { _id: 0, __v: 0 };
+        const { id, name, fullName, shortName, lessons } = await Teachers.findOne(query, projection);
+        const { weeks } = jsonProcessing.createTimetable(lessons);
+
+        expect(mongoose.connection.readyState).toEqual(1); //1: connected
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({ id, name, fullName, shortName, weeks });
+    });
+
+    it('GET /teachers/:id/timetable (by id) - error', async () => {
+        const res = await request(app).get('/api/teachers/-999999/timetable');
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual({
+            'message': 'Teacher not found',
+            'statusCode': 404
+        });
+    });
+
+    it('GET /teachers/:name/timetable (by name) - success', async () => {
+        const res = await request(app).get(`/api/teachers/${encodeURI('Болдак Андрій Олександрович')}/timetable`); //uses the request function that calls on express app instance
+        const query = { $or: [{ name: 'Болдак Андрій Олександрович' }] };
+        const projection = { _id: 0, __v: 0 };
+        const { id, name, fullName, shortName, lessons } = await Teachers.findOne(query, projection);
+        const { weeks } = jsonProcessing.createTimetable(lessons);
+
+        expect(mongoose.connection.readyState).toEqual(1); //1: connected
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual({ id, name, fullName, shortName, weeks });
+    });
+
+    it('GET /teachers/:name/timetable (by name) - error', async () => {
+        const res = await request(app).get('/api/teachers/some-unexpected-name/timetable');
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual({
+            'message': 'Teacher not found',
+            'statusCode': 404
+        });
+    });
 });
